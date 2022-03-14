4e069337ff80c985bc10b485e55548f5eac047ad
Praveen210898 <praveenmaddy22@gmail.com>
2022-03-14 21:47:08 +0530

    praveencode

---
+++ src/test/java/org/test/sampletest.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class sampletest {
+
+public static void main(String[] args) {
+	sampletest s=new sampletest();
+	s.methoda();
+	
+	
+	
+	
+}
+
+private void methoda() {
+	System.out.println("his name is praveen");
+
+
+}
+}
