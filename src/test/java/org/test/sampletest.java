b61135f084dc267f88e31b215642a1ce1df1adb2
Praveen210898 <praveenmaddy22@gmail.com>
2022-03-14 21:59:27 +0530

    praveencode

--- src/test/java/org/test/sampletest.java
+++ src/test/java/org/test/sampletest.java
@@ -2,18 +2,25 @@ package org.test;
 
 public class sampletest {
 
-public static void main(String[] args) {
-	sampletest s=new sampletest();
-	s.methoda();
-	
-	
-	
-	
-}
-
-private void methoda() {
-	System.out.println("his name is praveen");
+	public static void main(String[] args) {
+		sampletest s=new sampletest();
+		s.methoda();
+        s.methodb();
 
 
-}
+
+	}
+
+	private void methoda() {
+		System.out.println("his name is praveen");
+	}
+
+	private void methodb() {
+		System.out.println("his name is vani");
+
+	}
+
+
+
+
 }
